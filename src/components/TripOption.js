5111aaef133a8a5b291654acac79f4d189c9713e
Lucas de Assis Rosa <lucasassis413@gmail.com>
2019-02-05 23:23:01 -0200

    add style for different product status

--- src/components/TripOption.js
+++ src/components/TripOption.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import './TripOption.css';
 
+const sortProductsById = (products = []) =>
+  products.sort((a, b) => Number(a.productClassId) - Number(b.productClassId));
+
 const getPreviousValue = price => (
   price.rrp !== price.rrpWithDiscount
     ? (
@@ -24,20 +27,23 @@ export const TripOption = ({ trip }) => (
       <div className="trip-option-header-temp">{trip.temperature}</div>
     </div>
     {
-      trip.products.map(({ productClass, prices, status }) => (
-        <div key={productClass} className="trip-option-product">
-          <div className="trip-option-product-value">
-            {getPreviousValue(prices[0])}
-            <div className="trip-option-product-actual">
-              {prices[0].currencySymbol}
-              {prices[0].rrpWithDiscount}
+      sortProductsById(trip.products)
+        .map(({ productClass, prices, status, statusCode }) => (
+          <div
+            key={productClass}
+            className={`trip-option-product trip-option-product-${statusCode}`}>
+            <div className="trip-option-product-value">
+              {getPreviousValue(prices[0])}
+              <div className="trip-option-product-actual">
+                {prices[0].currencySymbol}
+                {prices[0].rrpWithDiscount}
+              </div>
+            </div>
+            <div className={`trip-option-product-status trip-option-product-status-${statusCode}`}>
+              {status}
             </div>
           </div>
-          <div className="trip-option-product-status">
-            {status}
-          </div>
-        </div>
-      ))
+        ))
     }
   </div>
 );
@@ -49,7 +55,9 @@ TripOption.propTypes = {
     temperature: PropTypes.string,
     products: PropTypes.arrayOf(PropTypes.shape({
       productClass: PropTypes.string,
+      productClassId: PropTypes.string,
       status: PropTypes.string,
+      statusCode: PropTypes.string,
       prices: PropTypes.array,
     })),
   }).isRequired,
