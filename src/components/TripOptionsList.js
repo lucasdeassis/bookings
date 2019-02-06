a24e8410abaffadfe8c8ee876bff0818e847b117
Lucas de Assis Rosa <lucasassis413@gmail.com>
2019-02-05 23:23:12 -0200

    add product classes label on table

--- src/components/TripOptionsList.js
+++ src/components/TripOptionsList.js
@@ -6,8 +6,22 @@ import './TripOptionsList.css';
 
 const MAX_OPTIONS_PER_TIME = 8;
 
+const PRODUCTS_CLASSES = [
+  "Premier",
+  "Premier Plus",
+  "Catamaran"
+];
+
 const TripOptionsList = ({ trips }) => (
   <div className="trip">
+    <div className="trip-option">
+      <div />
+      {
+        PRODUCTS_CLASSES.map(productClass => (
+          <div className="trip-option-label">{productClass}</div>
+        ))
+      }
+    </div>
     {
       [...Array(MAX_OPTIONS_PER_TIME)].map((_, index) => (
         <TripOption
